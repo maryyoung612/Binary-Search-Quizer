cf829684655612b41609d2d809ab1154dc6796cb
Mary.Young <Mary.Young@MU-C1-07.mullen.pvt.k12.co.us>
2015-11-19 09:10:01 -0700

    bro

--- Binary Search Quiz/src/Runner.java
+++ Binary Search Quiz/src/Runner.java
@@ -3,12 +3,41 @@ import java.util.Scanner;
 public class Runner
 	{
 	static int arrayNum;
+	static int guessL;
+	static int guessR;
+	static int guessM;
 	public static void main(String[] args)
 		{
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Welcome to the Binary Search Quizer! You will be tested on the logic of the search,");	
 		System.out.println(" so you will need to tell me what the left, right, and middle are in each step.");
 		System.out.println("                       ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
 		Searcher.genRandomArray();
+		System.out.println("The target is "+Searcher.target);
+		Boolean ghost=true;
+		while (ghost=true)
+			{
+			System.out.println("~~~~~~~~~~~Pass~~~~~~~~~~~~");
+			Searcher.binarySearch(Searcher.elements, Searcher.target);
+			Runner.askQuestion();
+			System.out.println("You have gotten "+(Searcher.counterR+Searcher.counterR2+Searcher.counterR3)+" correct and "+(Searcher.counterW+Searcher.counterW2+Searcher.counterW3)+" wrong.");
+			}
+		}
+	public static void askQuestion()
+		{
+		Scanner userInput = new Scanner(System.in);
+		System.out.println("Here is the array of ordered numbers, what would the position of the left be?");
+		guessL=userInput.nextInt();
+		Searcher.checkLeftAnswer();
+		System.out.println("                       ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("What would the position of the right be?");
+		guessR=userInput.nextInt();
+		Searcher.checkRightAnswer();
+		System.out.println("                       ~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("What would the position of the middle be?");
+		guessM=userInput.nextInt();
+		Searcher.checkMiddleAnswer();
+		System.out.println("                       ~~~~~~~~~~~~~~~~~~~~~~~~~~");
 		}
 	}
--- Binary Search Quiz/src/Searcher.java
+++ Binary Search Quiz/src/Searcher.java
@@ -1,24 +1,40 @@
-import java.util.Scanner;
+import java.util.Arrays;
 public class Searcher
 	{
-	static int elements[]=new int[0];
+	static int elements[]=new int[8];
+	static int left;
+	static int right;
+	static int middle;
+	static int target;
+	static int random;
+	static int counterR;
+	static int counterW;
+	static int counterR2;
+	static int counterW2;
+	static int counterR3;
+	static int counterW3;
 	public static void genRandomArray()
 	{
 	for(int i=0; i<elements.length;i++)
 		{
-		elements[i] =(int)(Math.random()*12)+2;
-		System.out.println(elements[i]);
-		
+		elements[i] =(int)(Math.random()*12)+1;
+		System.out.print(elements[i]);
+		System.out.print(" ");
+		Arrays.sort(elements);
 		}
-	System.out.println("jack");
+	System.out.println();
+	random=(int)(Math.random()*7)+1;
+	System.out.println(random);
+	target=elements[random];
 	}
+	
 	public static int binarySearch(int[]elements, int target)
 		{
-		int left=0;
-		int right = elements.length-1;
+		left=0;
+		right = elements.length-1;
 		while(left<=right)
 			{
-			int middle =(left+right)/2;
+			middle =(left+right)/2;
 			if(target<elements[middle])
 				{
 				right=middle-1;
@@ -33,5 +49,50 @@ public class Searcher
 				}
 			}
 		return -1;
+		}
+	public static void checkLeftAnswer()
+		{
+		counterR=0;
+		counterW=0;
+		if(Runner.guessL==Searcher.left)
+			{
+			System.out.println("Good job, "+Runner.guessL+" is the correct answer!");
+			counterR++;
+			}
+		else
+			{
+			System.out.println("That is wrong. The correct answer is "+left);
+			counterW++;
+			}
+		}
+	public static void checkRightAnswer()
+		{
+		counterR2=0;
+		counterW2=0;
+		if(Runner.guessR==Searcher.right)
+			{
+			System.out.println("Good job, "+Runner.guessR+" is the correct answer!");
+			counterR2++;
+			}
+		else
+			{
+			System.out.println("That is wrong. The correct answer is "+right);
+			counterW2++;
+			}
+		}
+	public static void checkMiddleAnswer()
+		{
+		counterR3=0;
+		counterW3=0;
+		if(Runner.guessM==Searcher.middle)
+			{
+			System.out.println("Good job, "+Runner.guessM+" is the correct answer!");
+			counterR3++;
+			}
+		else
+			{
+			System.out.println("That is wrong. The correct answer is "+middle);	
+			counterW3++;
+			}
 		}
 	}
